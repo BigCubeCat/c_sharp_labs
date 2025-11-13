8616adb1d79d102b0d48a3aff2ea62ef162c8e4c
Bitochkin Egor <bigcubecat@yandex.ru>
2025-11-14 05:41:00 +0700

    lab4 finished

--- lab3/Interface/obj/Debug/net9.0/Interface.AssemblyInfo.cs
+++ lab3/Interface/obj/Debug/net9.0/Interface.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Interface")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fff1f02d17068b0710a3e2156a10c9868abf241b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e243d0ccf53add28e350cfc78ae6b8235b474bf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Interface")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Interface")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
